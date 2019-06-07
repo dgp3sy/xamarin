b59f0e92aa5ed1bbd5c6a7542a8a7a30b76dc5d9
Danny Perkins <48068870+dgp3sy@users.noreply.github.com>
2019-06-07 08:46:18 -0500

    Add files via upload

---
+++ DatabaseConnection_Android.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+using NotePage.Droid;
+
+[assembly: Xamarin.Forms.Dependency(typeof(DatabaseConnection_Android))]
+
+namespace NotePage.Droid
+{
+    class DatabaseConnection_Android : IDatabaseConnection
+    {
+        public SQLiteConnection DbConnection()
+        {
+            var path = "C:\\Users\\danny.perkins\\source\\repos\\NotePage\\NotePage\\NotePage.Android\\Assets\\my_table.db";
+            return new SQLiteConnection(path);
+        }
+    }
+}
